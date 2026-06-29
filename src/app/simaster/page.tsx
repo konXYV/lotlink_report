30339538c096774f5d2f81fb90787820a0925987
sakon04573 <sakon04573@sokxaygroup.local>
2026-06-29 11:11:47 +0700

    v3-29-06-2026-

---
+++ src/app/simaster/page.tsx
@@ -0,0 +1,16 @@
+// src/app/page.tsx
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh',
+      fontFamily: 'sans-serif' 
+    }}>
+      <h1>Hello simaster🚀</h1>
+    </main>
+  );
+}
